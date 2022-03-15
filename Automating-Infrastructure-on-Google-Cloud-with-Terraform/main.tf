ba223b8fa9051cac91d94639ab13864a932324c4
gerray <wyq0213@gmail.com>
2022-03-15 09:25:09 +0900

    faiture追加 terraform

---
+++ Automating-Infrastructure-on-Google-Cloud-with-Terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.13.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+module "instances" {
+  source       = "./modules/instances"
+  machine_type = var.machine_type
+  zone         = var.zone
+}
---
+++ Automating-Infrastructure-on-Google-Cloud-with-Terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  default = "qwiklabs-gcp-04-3c8da3f4263c"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
